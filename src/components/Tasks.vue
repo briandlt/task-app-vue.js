ac94f5a6131b62f6231df3fc04c2ff0df50618db
briandlt <brianesparzadlt4@gmail.com>
2019-09-09 13:27:09 -0500

    iniciamos!

---
+++ src/components/Tasks.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>{{titulo}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'tasks',
+    data() {
+        return {
+            titulo: "HOLA MUNDO"
+        }
+    },
+
+}
+</script>
+
+<style scope="">
+
+</style>

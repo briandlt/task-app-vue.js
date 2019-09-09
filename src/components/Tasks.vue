efa2fbfb0ea5f3269b8a515ff6d922fb8435e769
briandlt <brianesparzadlt4@gmail.com>
2019-09-09 16:44:01 -0500

    Terminamos nusestra aplicacion de administrador de tareas en vue.js

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,6 +1,32 @@
 <template>
-    <div>
-        <h1>{{titulo}}</h1>
+    <div class="container">
+        <h3 class="mb-4">{{title}}</h3>
+        <form action="" class="form-inline my-4" v-on:submit="addTask">
+            <div class="m-auto">
+                <input type="text" name="" v-model="tasks.title" class="form-control">
+                <input type="submit" value="Agregar" class="btn btn-success mx-2">
+            </div>
+        </form>
+        <table class="table col-sm-8 offset-sm-2">
+            <thead>
+                <tr>
+                    <th>Hecho!</th>
+                    <th>Tarea</th>
+                    <th>Eliminar</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="task in tasks">
+                    <td>
+                        <input type="checkbox" name="hecho" id="hecho" v-model="task.done" checked>
+                    </td>
+                    <td>
+                        <span :class="{taskDone: task.done}">{{task.name}}</span>
+                    </td>
+                    <td><input type="button" value="Eliminar" class="btn btn-danger" v-on:click="deleteTask(task)"></td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
@@ -9,7 +35,38 @@ export default {
     name: 'tasks',
     data() {
         return {
-            titulo: "HOLA MUNDO"
+            title: "TASK APP",
+            tasks:[
+                {
+                    name: 'Correr',
+                    done: true
+                },
+                {
+                    name: 'Comer',
+                    done: false
+                },
+                {
+                    name: 'Lavar',
+                    done: false
+                },
+                {
+                    name: 'Leer ',
+                    done: false
+                }
+            ]
+        } 
+    },
+    methods: {
+        addTask: function(e){
+            e.preventDefault();
+            this.tasks.push({
+                name: this.tasks.title,
+                done: false
+            });
+            this.tasks.title = "";
+        },
+        deleteTask: function(task){
+            this.tasks.splice(this.tasks.indexOf(task), 1);
         }
     },
 
@@ -17,5 +74,7 @@ export default {
 </script>
 
 <style scope="">
-
+    .taskDone{
+        text-decoration: line-through;
+    }
 </style>
